a4c81fab64e0a1bb375d7d36845962089b6bb1b8
HuaYumian <976290015@qq.com>
2016-12-07 00:11:38 +0800

    搭建框架

--- DataStructSection1/OrderList.c
+++ DataStructSection1/OrderList.c
@@ -1,7 +1,45 @@
 #include "Includes.h"
 
+/**
+ *@topic:  线性表的顺序表示和实现 
+ *@date:   2016/12/07
+ *@note: 
+ */
 
+/**
+ *@Function:Creat a dynamic order list 
+ *@params:
+ *@return:
+ *@note:
+ */
+bool Init_OrderList()
+{
+	
+}
 
-
-
-
+/**
+ *@Function:Insert an element to the order list,增 
+ *@params:
+ *@return:
+ *@note:
+ */
+ bool Insert_OrderList()
+ {
+ 	
+ } 
+ 
+ /**
+ *@Function:Delete an element from the order list,删 
+ *@params:
+ *@return:
+ *@note:
+ */
+ bool Delete_OrderList()
+ {
+ 	
+ }
+ 
+ 
+ 
+ 
+ 
